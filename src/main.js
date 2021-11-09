2033d8d4f1012ab5a74fa093cb2e4aa693d6f77b
mohamed <mohamedhalouy@gmail.com>
2021-11-10 00:06:08 +0100

    libreria AOS integrada

--- src/main.js
+++ src/main.js
@@ -12,6 +12,11 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
 Vue.config.productionTip = false
 
 new Vue({
