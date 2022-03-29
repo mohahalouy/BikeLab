151004dbecea5014796ec640b0ea54ab10457de8
Moha <mohamedhalouy@gmail.com>
2022-03-29 22:47:31 +0200

    maquetacion en la visualizacion de los modelos

--- src/views/Modelos.vue
+++ src/views/Modelos.vue
@@ -4,13 +4,15 @@
     <div class="fullDisplayHeader">
       <FullDisplayHeader></FullDisplayHeader>
     </div>
-    <section class="modelo row w-100">
-      <div class="col-2 mx-2 my-2" v-for="item in this.dataModelos" :key="item.id">
-        <picture class="imgNoticia">
-          <img class="img-fluid" :src="'http://127.0.0.1:8000/uploads/modelos/imagenes/'+item.imagen">
-        </picture>
-        <h2>{{ item.nombreEs }}</h2>
-        <p>{{ item.destacadoEs }}</p>
+    <section class="modelo  w-100 d-flex justify-content-center">
+      <div class="w-75 d-flex my-3 flex-wrap">
+        <div class="" v-for="item in this.dataModelos" :key="item.id">
+            <picture class="imgNoticia">
+              <img class="img-fluid" :src="'http://127.0.0.1:8000/uploads/modelos/imagenes/'+item.imagen">
+            </picture>
+            <h2>{{ item.nombreEs }}</h2>
+            <p>{{ item.destacadoEs }}</p>
+        </div>
       </div>
     </section>
     <Footer class="footer"></Footer>
@@ -82,9 +84,26 @@ export default {
   background-color: #e6e6e6;
 }
 
-.modelo > div{
+.modelo > div > div{
   background-color: white;
-  height: fit-content;
+  min-height: 200px;
+  cursor: pointer;
+  padding: 10px 20px;
+  max-width: 220px;
+  margin-top: 10px;
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.modelo > div > div img {
+  padding: 20px;
+  transition: transform 0.5s;
+}
+
+.modelo > div > div:hover img {
+  transform: scale(1.1);
 }
 
 </style>
